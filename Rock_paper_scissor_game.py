9760cb091aacc60a61ec4c7abc11d1096458e8c0
Kavyashreesuvarna <kavyashreek526@gmail.com>
2026-01-01 23:34:23 +0530

    Add files via upload

---
+++ Rock_paper_scissor_game.py
@@ -0,0 +1,53 @@
+'''
+Work flow of the project
+1-Input from user(Rock, paper, scissor)
+2-Computer choice (Computer will choose randomly not conditionally)
+3-Result print
+
+Cases:
+A- Rock
+Rock - Rock = tie
+Rock - Paper = Paper win
+Rock - Scissor = Rock win
+
+B- Paper
+Paper - Paper = tie
+Paper - Rock = Paper win
+Paper - Scissor = Scissor win
+
+C- Scissor
+Scissor - Scissor = tie
+Scissor - Rock = Rock win
+Scissor - Paper = Scissor win
+'''
+
+import random
+
+item_list=["Rock", "Paper", "Scissor"]
+
+user_choice=input("Enter your choice = Rock, paper,Scissor = ")
+comp_choice=random.choice(item_list)
+
+print(f"User choice = {user_choice}, computer choice = {comp_choice}")
+
+
+if user_choice == comp_choice:
+    print("Both chooses same: Match tie")
+
+elif user_choice == "Rock":
+    if comp_choice == "Paper":
+        print("Paper covers Rock = Computer win")
+    else:
+        print("Rock smashes scissor = You win")
+
+elif user_choice == "Paper":
+    if comp_choice == "Rock":
+        print("Paper covers Rock You win")
+    else:
+        print("Scissor cuts paper so computer wins")
+
+elif user_choice == "Scissor":
+    if comp_choice == "Rock":
+        print("Rock smashes Scissor Computer win")
+    else:
+        print("Scissor cuts paper you wins")
